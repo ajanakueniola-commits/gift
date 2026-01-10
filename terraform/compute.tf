69cd84bc1c995c240f48587dad5d8272bbc6df59
Funmi123 <ajanakueniola@gmail.com>
2026-01-10 11:23:35 +0100

    nice

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,17 +1,17 @@
-resource "aws_instance" "web-server" {
+resource "aws_instance" "web" {
   ami           = var.web-server-ami_id
   instance_type = var.instance_type
   subnet_id     = aws_subnet.public.id
-  vpc_security_group_ids = [aws_security_group.web-server.id]
+  vpc_security_group_ids = [aws_security_group.web.id]
 
   tags = { Name = "grace-web-server" }
 }
 
-resource "aws_instance" "backend-server" {
+resource "aws_instance" "backend" {
   ami           = var.backend-server-ami_id
   instance_type = var.instance_type
   subnet_id     = aws_subnet.private.id
-  vpc_security_group_ids = [aws_security_group.backend-server.id]
+  vpc_security_group_ids = [aws_security_group.backend.id]
 
   tags = { Name = "grace-backend-server" }
 }
