ec72b36b647045922802b0b5a253b065b881790f
Funmi123 <ajanakueniola@gmail.com>
2026-01-10 10:10:23 +0100

    updates commits

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami           = var.nginx_ami_id
+  ami           = var.web-server-ami_id
   instance_type = var.instance_type
   subnet_id     = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web.id]
@@ -8,7 +8,7 @@ resource "aws_instance" "web" {
 }
 
 resource "aws_instance" "app" {
-  ami           = var.python_ami_id
+  ami           = var.backend-server-ami_id
   instance_type = var.instance_type
   subnet_id     = aws_subnet.private.id
   vpc_security_group_ids = [aws_security_group.app.id]
@@ -17,7 +17,7 @@ resource "aws_instance" "app" {
 }
 
 resource "aws_instance" "db" {
-  ami           = var.python_ami_id
+  ami           = var.backend-server-ami_id
   instance_type = var.instance_type
   subnet_id     = aws_subnet.private.id
   vpc_security_group_ids = [aws_security_group.db.id]
