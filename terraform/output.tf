278b26704daf31309805d12226baf742fd84beb3
Funmi123 <ajanakueniola@gmail.com>
2026-01-10 11:51:30 +0100

    nice

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,33 +1,18 @@
+# outputs.tf
+
+output "web_server_ips" {
+  value = [aws_instance.web.public_ip]
+}
+
+output "backend_server_ips" {
+  value = [aws_instance.backend.public_ip]
+}
+
+output "db_server_ips" {
+  value = [aws_instance.db.private_ip]
+}
+
+# Remove or comment out ALB outputs if no ALB is created
 # output "alb_dns" {
 #   value = aws_lb.web.dns_name
 # }
-
-output "web-server_ip" {
-  value = aws_instance.web.public_ip
-}
-
-output "backend-server_ip" {
-  value = aws_instance.backend.private_ip
-}
-
-output "db_ip" {
-  value = aws_instance.db.private_ip
-}
-output "vpc_id" {
-  value = aws_vpc.grace.id
-}
-output "public_subnet_id" {
-  value = aws_subnet.public.id
-}
-output "private_subnet_id" {
-  value = aws_subnet.private.id
-}
-output "security_group_web_id" {
-  value = aws_security_group.web.id
-}
-output "security_group_backend_id" {
-  value = aws_security_group.backend.id
-}
-output "security_group_db_id" {
-  value = aws_security_group.db.id
-}
