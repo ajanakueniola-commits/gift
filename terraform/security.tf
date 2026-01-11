84528bf25fb4997772ab090bc96275ab46125122
Funmi123 <ajanakueniola@gmail.com>
2026-01-11 06:57:42 +0100

    updated repo

--- terraform/security.tf
+++ terraform/security.tf
@@ -22,16 +22,9 @@ resource "aws_security_group" "web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
 resource "aws_security_group" "backend" {
+  name   = "grace-backend-sg"
   vpc_id = aws_vpc.grace.id
-
-  ingress {
-    from_port       = 5000
-    to_port         = 5000
-    protocol        = "tcp"
-    security_groups = [aws_security_group.backend.id]
-  }
 
   egress {
     from_port   = 0
@@ -42,22 +35,59 @@ resource "aws_security_group" "backend" {
 }
 
 resource "aws_security_group" "db" {
+  name   = "grace-db-sg"
   vpc_id = aws_vpc.grace.id
 
-  ingress {
-    from_port       = 5432
-    to_port         = 5432
-    protocol        = "tcp"
-    security_groups = [aws_security_group.db.id]
-  }
-
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+
+
+
+
+
+
+
+# resource "aws_security_group" "backend" {
+#   vpc_id = aws_vpc.grace.id
+
+#   ingress {
+#     from_port       = 5000
+#     to_port         = 5000
+#     protocol        = "tcp"
+#     security_groups = [aws_security_group.backend.id]
+#   }
+
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
+
+# resource "aws_security_group" "db" {
+#   vpc_id = aws_vpc.grace.id
+
+#   ingress {
+#     from_port       = 5432
+#     to_port         = 5432
+#     protocol        = "tcp"
+#     security_groups = [aws_security_group.db.id]
+#   }
+
+#     egress {
+#         from_port   = 0
+#         to_port     = 0
+#         protocol    = "-1"
+#         cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
 resource "aws_db_subnet_group" "grace" {
   name = "grace-db-subnet-group"
