ac6593d76f894a721c8d86a18038a75dde1badb1
Funmi123 <ajanakueniola@gmail.com>
2026-01-11 02:38:53 +0100

    updated repo

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -13,6 +13,17 @@ variable "instance_type" {
   default = "c7i-flex.large"
 }
 
-variable "postgres_ami" {
-  description = "Postgres AMI ID created by Packer"
+  variable "postgres_ami" {
+    description = "Postgres AMI ID created by Packer"
+  }
+
+variable "db_name" {
+  description = "Name of the database"
+}
+
+variable "db_username" {
+  description = "Username for the database"
+}
+variable "db_password" {
+  description = "Password for the database"
 }
